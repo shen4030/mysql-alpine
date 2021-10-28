0866e06881f1d10df08bfa2242c29c207454e7e7
brucewayne91 <994318796@qq.com>
2021-10-28 16:34:27 +0800

    update

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM alpine:latest
+
+ENV TIMEZONE Asia/Shanghai
+
+RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories
+RUN apk update \
+    && apk upgrade \
+    && apk add \
+    tzdata \
+    mysql \
+    mysql-client \
+    && cp /usr/share/zoneinfo/${TIMEZONE} /etc/localtime \
+    && echo "${TIMEZONE}" > /etc/timezone \
+    && apk del tzdata \
+    && rm -rf /var/cache/apk/*
+
+#COPY conf/my.cnf /etc/my.cnf
+COPY startup.sh /startup.sh
+RUN chmod +x /startup.sh
+
+EXPOSE 3306
+
+CMD ["/startup.sh"]
---
+++ startup.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+if [ ! -d "/run/mysqld" ]; then
+  mkdir -p /run/mysqld
+fi
+
+if [ -d /var/lib/mysql/mysql ]; then
+    echo "[i] MySQL directory already present, skipping creation"
+else
+    echo "[i] MySQL data directory not found, creating initial DBs"
+
+    mysql_install_db --user=root --datadir=/var/lib/mysql > /dev/null
+
+    if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
+        MYSQL_ROOT_PASSWORD=root
+        echo "[i] MySQL root Password: $MYSQL_ROOT_PASSWORD"
+    fi
+
+    MYSQL_DATABASE=${MYSQL_DATABASE:-""}
+    MYSQL_USER=${MYSQL_USER:-""}
+    MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
+
+    tfile=`mktemp`
+    if [ ! -f "$tfile" ]; then
+        return 1
+    fi
+
+    cat << EOF > $tfile
+USE mysql;
+FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY "$MYSQL_ROOT_PASSWORD" WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY "$MYSQL_ROOT_PASSWORD" WITH GRANT OPTION;
+EOF
+
+    if [ "$MYSQL_DATABASE" != "" ]; then
+        echo "[i] Creating database: $MYSQL_DATABASE"
+        echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
+
+        if [ "$MYSQL_USER" != "" ]; then
+            echo "[i] Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
+            echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
+            echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
+        fi
+    fi
+    sed -ri -e 's!#bind-address=0.0.0.0!bind-address=0.0.0.0!g' /etc/my.cnf.d/mariadb-server.cnf
+    /usr/bin/mysqld --user=root --bootstrap --verbose=0 < $tfile
+    rm -f $tfile
+fi
+
+exec /usr/bin/mysqld --user=root --console
